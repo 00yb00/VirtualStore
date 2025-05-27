97d85087671f31db5bb0ce9f3532ed50be4aed64
yaffa <boaronyaffa@gmail.com>
2025-05-27 20:59:46 +0300

    fix

---
+++ VirtualStoreDapr/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VirtualStore.Managers.Models
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public int? SupplierID { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public string? QuantityPerUnit { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        public short? UnitsInStock { get; set; }
+
+        public short? UnitsOnOrder { get; set; }
+
+        public short? ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+}
